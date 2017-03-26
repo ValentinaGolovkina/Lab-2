01c1969a64cb5dce29ac42efddd4291508c12052
Валентина <@ASUS>
2017-03-26 16:01:57 +0300

    Создание списка и  тест count

---
+++ Lab-2-Test/DoubleLinkedListTDDTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab_2;
+
+namespace Lab_2_Test
+{
+    [TestClass]
+    public class DoubleLinkedListTDDTest
+    {
+        private DoubleLinkedListTDD<int> list;
+
+        [TestInitialize]
+        public void tdd_initializeTest()
+        {
+            list = new DoubleLinkedListTDD<int>();
+        }
+
+        [TestMethod]
+        public void tdd_getCountTest()
+        {
+            Assert.AreEqual(0, list.Count());
+        }
+    }
+}
