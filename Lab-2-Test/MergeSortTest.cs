016e66ad6965712eb0fad71f67557eaac98eb23a
Валентина <@ASUS>
2017-03-26 15:49:55 +0300

    Белый и черный ящик

---
+++ Lab-2-Test/MergeSortTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab_2;
+
+namespace Lab_2_Test
+{
+    [TestClass]
+    public class MergeSortTest
+    {
+        //Черный ящик
+        [TestMethod]
+        public void b_mergeSortOne()
+        {
+            int[] source = { 2 };
+            int[] expected = { 2 };
+            MergeSort mS = new MergeSort();
+            int[] actual = mS.mergeSort(source, 0, 0);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void b_mergeSortEven()
+        {
+            int[] source = { 2, 1, 0, 6, -1, 9, 8, 7 };
+            int[] expected = { -1, 0, 1, 2, 6, 7, 8, 9 };
+            MergeSort mS = new MergeSort();
+            int[] actual = mS.mergeSort(source, 0, 7);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void b_mergeSortOdd()
+        {
+            int[] source = { 2, 1, 0, 6, -1, 9, 8, 7, 3 };
+            int[] expected = { -1, 0, 1, 2, 3, 6, 7, 8, 9 };
+            MergeSort mS = new MergeSort();
+            int[] actual = mS.mergeSort(source, 0, 8);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void b_mergeSortPrecondition()
+        {
+            int[] source = { 4, 2, 5 };
+            MergeSort mS = new MergeSort();
+            int[] actual = mS.mergeSort(source, 2, 0);
+
+        }
+
+        //Белый ящик
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void w_mergeSortPath1()
+        {
+            int[] source = { 4, 2, 5 };
+            MergeSort mS = new MergeSort();
+            int[] actual = mS.mergeSort(source, 2, 0);
+        }
+        [TestMethod]
+        public void w_mergeSortPath2()
+        {
+            int[] source = { 2 };
+            int[] expected = { 2 };
+            MergeSort mS = new MergeSort();
+            int[] actual = mS.mergeSort(source, 0, 0);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void w_mergeSortPath3()
+        {
+            int[] source = { 2, 1 };
+            int[] expected = { 1, 2 };
+            MergeSort mS = new MergeSort();
+            int[] actual = mS.mergeSort(source, 0, 1);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void w_mergePath1()
+        {
+            int[] sourceA = { 1, 3 };
+            int[] sourceB = { 2 };
+            int[] expected = { 1, 2, 3 };
+            MergeSort mS = new MergeSort();
+            int[] actual = mS.merge(sourceA, 0, 1, sourceB, 0, 0);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void w_mergePath2()
+        {
+            int[] sourceA = { 1 };
+            int[] sourceB = { 2, 3 };
+            int[] expected = { 1, 2, 3 };
+            MergeSort mS = new MergeSort();
+            int[] actual = mS.merge(sourceA, 0, 0, sourceB, 0, 1);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void w_mergePath3()
+        {
+            int[] sourceA = { 1 };
+            int[] sourceB = { 2 };
+            int[] expected = { 1, 2 };
+            MergeSort mS = new MergeSort();
+            int[] actual = mS.merge(sourceA, 0, 0, sourceB, 0, 0);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void w_mergePath4()
+        {
+            int[] sourceA = { 2 };
+            int[] sourceB = { 1 };
+            int[] expected = { 1, 2 };
+            MergeSort mS = new MergeSort();
+            int[] actual = mS.merge(sourceA, 0, 0, sourceB, 0, 0);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Lab-2/MergeSort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    public class MergeSort
+    {
+        public int[] merge(int[] a, int startA, int endA, int[] b, int startB, int endB)
+        {
+            int aLength = endA - startA + 1;
+            int bLength = endB - startB + 1;
+
+            int[] c = new int[aLength + bLength];
+
+            int ai = 0;
+            int bi = 0;
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (ai >= aLength)
+                {
+                    c[i] = b[bi++];
+                    continue;
+                }
+                if (bi >= bLength)
+                {
+                    c[i] = a[ai++];
+                    continue;
+                }
+                if (a[ai] < b[bi])
+                {
+                    c[i] = a[ai++];
+                }
+                else
+                {
+                    c[i] = b[bi++];
+                }
+            }
+            return c;
+        }
+
+
+        public int[] mergeSort(int[] a, int start, int end)
+        {
+            if (start > end) throw new ArgumentException();
+            int length = end - start + 1;
+
+            if (length == 1)
+            {
+                return new int[] { a[start] };
+            }
+
+            if (length == 2)
+            {
+                if (a[start] < a[end])
+                {
+                    return new int[] { a[start], a[end] };
+                }
+                {
+                    return new int[] { a[end], a[start] };
+                }
+            }
+
+            int half = length / 2;
+
+            int[] b = mergeSort(a, start, start + half - 1);
+            int[] c = mergeSort(a, start + half, end);
+
+            return merge(b, 0, b.Length - 1, c, 0, c.Length - 1);
+        }
+    }
+}
